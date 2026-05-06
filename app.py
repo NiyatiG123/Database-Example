ccd728b11f64de3850dd803e09cd55dca282b34d
Your Name <you@example.com>
2026-05-07 11:00:31 +1200

    completed fighter database program

--- app.py
+++ app.py
@@ -1,9 +1,34 @@
+#docstring- Niyati Gupta- airb=plane database application
+#imports
 import sqlite3
 
-db = sqlite3.connect("fighters.db")
-cursor = db.cursor()
-sql = "SELECT * from fighter;"
-cursor.execute(sql)
-results = cursor.fetchall()
-print(results)
-db.close()
+#constants and variables
+DATABASE = "fighters.db"
+
+
+#functions
+def print_all_aircraft():
+    '''print all the aircraft nicely'''
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "SELECT * FROM fighter;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    #loop through all the results
+    print(f"name                           speed   max_g climb range payload")
+    for fighter in results:
+        print(f"{fighter[1]:<30}{fighter[2]:<8}{fighter[3]:<6}{fighter[4]:<6}{fighter[5]:<6}{fighter[6]:<6}")
+    #loop finished here
+    db.close()
+
+
+
+#main code
+while True:
+    user_input = input("\nWhat would you like to do.\n1. Print all aircraft\n2.Exit\n")
+    if user_input == "1":
+        print_all_aircraft()
+    if user_input == "2":
+        break
+    else:
+        print("That was not an option\n")
